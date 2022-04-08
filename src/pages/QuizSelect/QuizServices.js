c0d3e20f621cd85cc992e93a2146e0c521d681e4
Richard <richardstuart007@hotmail.com>
2022-04-08 12:06:33 +1200

    new data

--- src/pages/QuizSelect/QuizServices.js
+++ src/pages/QuizSelect/QuizServices.js
@@ -4,12 +4,15 @@ export const getOwnerCollection = () => [
 ]
 
 export const getGroup1Collection = () => [
+  { id: '', title: 'none' },
   { id: 'Redoubles', title: 'Redoubles' },
   { id: '1NT', title: '1NT' },
   { id: 'Doubles', title: 'Doubles' },
   { id: 'Carol', title: 'Carol' },
   { id: 'WBF', title: 'WBF' },
   { id: 'TwoWayCheckback', title: 'TwoWayCheckback' },
+  { id: 'Quantitative4NT', title: 'Quantitative4NT' },
+  { id: 'Cue1st2ndControls', title: 'Cue1st2ndControls' },
   { id: 'BergenRaises', title: 'BergenRaises' },
   { id: 'Points Schmoints', title: 'Points Schmoints' }
 ]
